54ab98a9635a9fb8a918b4792af64c011aa3a97a
FireWolfy89 <dmate899@gmail.com>
2024-12-21 22:23:56 +0100

    Calculation and API Calls are written in the script file

---
+++ Exchange_Rate/exchangeCalc.js
@@ -0,0 +1,32 @@
+const API_KEY = "44300e08fb2c19431cc5ebe5";
+
+//Button
+let submitButton = document.querySelector(".exchangeButton");
+
+submitButton.addEventListener("click", async () => {
+  //Dropdowns
+  let fromSelect = document.getElementById("exchange1").value;
+  let toSelect = document.getElementById("exchange2").value;
+
+  //Inputs
+  let fromInput = document.getElementById("inputFrom").value;
+  let toInput = document.getElementById("inputTo").value;
+
+  const result = await fetchingData(fromSelect);
+
+  const rate = result[toSelect];
+
+  toInput.value = exchangeCalculation(fromInput,rate);
+});
+
+async function fetchingData(currencyType) {
+  const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currencyType}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.conversion_rates;
+}
+
+function exchangeCalculation(fromCurrency, rate) {
+  return fromCurrency * rate;
+}
